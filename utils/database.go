87e07ce91caac67d57bf7c7043baa24ecf204baa
Richard Trøndheim <richardtroendheim@gmail.com>
2018-07-23 21:22:30 +0200

    Avoid 502s

--- utils/database.go
+++ utils/database.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"database/sql"
 	"github.com/spf13/viper"
+	"time"
 )
 
 var Database *sql.DB
@@ -13,6 +14,8 @@ func OpenConnection() {
 	var error error
 
 	Database, error = sql.Open("mysql", viper.GetString("dsn"))
+
+	Database.SetConnMaxLifetime(time.Second * 10)
 
 	if error != nil {
 		log.Fatalf("Error on initializing database connection: %s", error.Error())
