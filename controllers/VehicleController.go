810431c4372b955dcf21f2b2222d71f6796b9d55
Richard Trøndheim <richardtroendheim@gmail.com>
2018-07-23 20:40:16 +0200

    Revert "Handle preflight cors"
    This reverts commit 98d1db4021fccddfbebc59d6023ce15142c8f9b9.

--- controllers/VehicleController.go
+++ controllers/VehicleController.go
@@ -6,19 +6,16 @@ import (
 	"motorregister-api/models/vehicle"
 	"fmt"
 	"github.com/bdwilliams/go-jsonify/jsonify"
-	"motorregister-api/utils"
 )
 
 // Get statistics for brand-model-zip
-func ModelZipStatistics(responseWriter http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	var isPreFlight = utils.SetCorsHeaders(request, responseWriter)
-	if isPreFlight {
-		ModelZipStatistics(responseWriter, request, params)
-		return
-	}
+func ModelZipStatistics(response http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
 	var models = models.GetModelCount(params.ByName("brand"), params.ByName("model"))
-	fmt.Fprint(responseWriter, jsonify.Jsonify(models))
+
+	response.Header().Set("Access-Control-Allow-Origin", "*")
+
+	fmt.Fprint(response, jsonify.Jsonify(models))
 }
 
 // Get possible models for brand
